529f863ba783e6085994de95281bc92db34b855c
RAGHUL V <raghul.v2005@gmail.com>
2026-05-03 08:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.54%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -4,10 +4,6 @@ class Solution:
         while(i<len(s))and(j<len(t)):
             if(s[i]==t[j]):
                 i+=1
-                j+=1
-            else:
-                j+=1
-        if(i==len(s)):
-            return True
-        return False
-        
+            j+=1
+            
+        return True if i==len(s) else False
