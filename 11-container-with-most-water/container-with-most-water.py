a220f475dc158779b4ba145effc9d0d08cfa378f
RAGHUL V <raghul.v2005@gmail.com>
2026-05-05 08:26:20 +0530

    Time: 67 ms (11.39%) | Memory: 29.6 MB (37.87%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -6,6 +6,9 @@ class Solution:
             area = max(min(height[i], height[j]) * (j - i), area)
             if height[i] < height[j]:
                 i += 1
+            elif height[i] > height[j]:
+                j -= 1
             else:
+                i += 1
                 j -= 1
         return area
